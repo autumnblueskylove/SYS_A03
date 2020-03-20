f2fbcaeb35ab05fabc071303c509aae412ef75a5
autumnblueskylove <iamfavored@hanmail.net>
2020-03-19 23:11:02 -0400

    add a enum

--- inc/dataCreator.h
+++ inc/dataCreator.h
@@ -10,3 +10,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+typedef enum
+{
+    OK,                             // means Everything is OKAY
+    FAILURE_HYDRAULIC_FAILURE,      // means Hydraulic Pressure Failure
+    FAILURE_SAFETY_BUTTON,          // means Safety Button Failure
+    NO_RAW_MATERIAL,                // means No Raw Material in the Process
+    OUT_OPERATING_TEMP,             // means Operating Temperature Out of Range
+    ERROR_OPERATOR,                 // means Operator Error
+    OFF_Line                        // means Machine is Off-line
+} MESSAGESTATUS;
