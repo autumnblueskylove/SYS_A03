41bed28bfdee8b4210e9745327842f795e6b4495
autumnblueskylove <iamfavored@hanmail.net>
2020-03-19 23:42:32 -0400

    Add DX

--- inc/dataCorruptor.h
+++ inc/dataCorruptor.h
@@ -10,3 +10,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <signal.h>
+
+#define     MAX_DC_ROLES    10
+
+#define     DO_NOTING       0
+#define     KILL_DC         1
+#define     DELETE_MSGQ     2
+
+typedef struct 
+{
+    pid_t   dcProcessID;
+    int     lastTimeHeardFrom;
+} DCInfo;
+
+
+typedef struct 
+{
+    int     msgQueueID;
+    int     numberOfDCs;
+    DCInfo  dc[MAX_DC_ROLES];
+} MasterList;
