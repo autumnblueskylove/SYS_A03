6480a35e9bd1bb5b3687c3cf45527d8a4da04ed3
autumnblueskylove <iamfavored@hanmail.net>
2020-03-21 21:44:06 -0400

    Hoochamacallit

---
+++ Hoochamacallit/Common/src/dataLogger.c
@@ -0,0 +1,124 @@
+/*
+ * File        : dataLogger.c
+ * Project     : Hoochmacallit
+ * By          : Hyungbum Kim and Charng Gwon Lee
+ * Date        : March 21, 2020
+ * Description : This program is to generate log files for DC, DR, and DX
+ *               using semaphore to prevent file crash.
+ *               It will be stored in tmp folder with seperate name
+ *               DC - /tmp/dataCreator.log
+ *               DR - /tmp/dataMonitor.log
+ *               DX - /tmp/dataCorruptor.log
+ *               Open designated files and check semaphore 
+ *               after writting log messages, close files and Exit semaphore
+ */
+
+#define __DATA_LOGGER_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <time.h>
+#include "../inc/dataLogger.h"
+
+
+// int main ()
+// {
+//     int semid;
+//     // get semaphore ID
+//     semid = semget (IPC_PRIVATE, 1, IPC_CREAT | 0666);
+//     if(semid == -1)
+//     {
+//         printf("(Logger) Cannot get semid\n");
+//         exit(1);
+//     }
+//     printf ("(Logger) semID is %d\n", semid);
+//     if(semctl(semid, 0, SETALL, init_values) == -1)
+//     {
+//          printf("(Logger) Cannot initialize semid\n");
+//         exit(2);
+//     }
+//     dlog(DATA_CORRUPTOR,semid,"Hello World");
+
+//     printf ("(Logger) Release the semaphores\n");
+//     semctl (semid, 0, IPC_RMID,0);
+//     return 1;
+// }
+
+
+/* =============================================================================*/
+/* Name		: dlog                              								*/
+/* PURPOSE  : to log each action of DC, DR, and DX Application					*/
+/*			  - set the default value											*/
+/* INPUTS   : progID     int	      - program ID of DC, DR, and DX    		*/
+/*            semid      int          - semaphore ID for cretical section       */
+/*            contents   char array   - message for writting log files          */
+/* RETURNS  : Nothing															*/
+/* =============================================================================*/
+
+void dlog(int progID, int semid, char contents[MAX_STRING_LOG])
+{
+    char filePath[MAX_STRING_LOG] = {""};
+    time_t 		    t;
+    FILE            *fp;
+    struct tm*      localTime; 
+
+
+    // get program ID of DC, DR, and DX
+    switch (progID)
+    {
+        case DATA_CREATOR:
+            {
+                strcpy(filePath,"/tmp/dataCreator.log");
+                break;
+            }
+        case DATA_MONITOR:
+            {
+                strcpy(filePath,"/tmp/dataMonitor.log");
+                break;
+            }
+        case DATA_CORRUPTOR:
+            {
+                strcpy(filePath,"/tmp/dataCorruptor.log");
+                break;
+            }
+    }
+
+    // Start critical region
+    if (semop (semid, &acquire_operation,1) == -1)
+    {
+        printf("(Logger) Cannot start critical region\n");
+        exit(1);
+    }
+
+    // open file for logging messages
+    if ((fp = fopen(filePath,"a+"))== NULL)
+    {
+        printf("(Logger) Cannot write to shared file\n");
+        semctl ( semid, 0, IPC_RMID);
+        exit (2);
+    }
+
+    //Time stamp for log file
+    t = time(NULL);
+    localTime = localtime(&t); 
+    //printf("Local time and date: %s\n", asctime(localTime)); 
+    fprintf(fp,"[%04d-%02d-%02d %02d:%02d:%02d] : ", (localTime->tm_year + 1900), (localTime->tm_mon + 1), localTime->tm_mday, localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
+
+    // write log message
+    fprintf(fp,"%s\n",contents);
+
+    // close flie
+    fclose(fp);
+
+    // exit semaphore
+    if(semop(semid, &release_operation,1) == -1)
+    {
+        printf("(Logger) Can't exit critical region\n");
+        semctl ( semid, 0, IPC_RMID);
+        exit (2);
+    }
+
+}
---
+++ Hoochamacallit/DX/src/dataCorruptor.c
@@ -0,0 +1,164 @@
+﻿/*
+ * File        : dataCorruptor.c
+ * Project     : Hoochmacallit
+ * By          : Hyungbum Kim and Charng Gwon Lee
+ * Date        : March 21, 2020
+ * Description : This program is to corrupt data between DC and DR.
+ *               Through killing DC process or deleting the message
+ *               queue being used between the set of DC and DR, create
+ *               ealternative (or exceptional) paths to test program
+ *               using WOD (Wheel of Destruction) 
+ *               - kill process
+ *               - delete message queue
+ *               - do nothing
+ */
+
+#include <sys/sem.h>
+#include "../inc/dataCorruptor.h"
+#include "../inc/dataLogger.h"
+
+int main() 
+{
+    int             shmid;
+    int             semid;
+    int             r;
+    key_t           shmem_key;
+    
+
+    // semaphore for logger
+    // get semaphore ID
+    semid = semget (IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    if(semid == -1)
+    {
+        //printf("(DX) Cannot get semid\n");
+        dlog(DATA_CORRUPTOR,semid,"(DX) Cannot get semid");
+        exit(1);
+    }
+    //printf ("(DX) semID is %d\n", semid);
+    if(semctl(semid, 0, SETALL, init_values) == -1)
+    {
+         //printf("(DX) Cannot initialize semid\n");
+         dlog(DATA_CORRUPTOR,semid,"(DX) Cannot initialize semid!");
+        exit(2);
+    }
+
+    // get key for shared memory
+    shmem_key = ftok(".", 16535);
+    if(shmem_key == -1)
+    {
+        //printf("(DX) Cannot allocate key\n");
+        dlog(DATA_CORRUPTOR,semid,"(DX) Cannot allocate key!");
+        // remove semaphore
+        semctl (semid, 0, IPC_RMID,0);
+        exit(3);
+    }
+    
+    // 100 retry to get shared memory
+    for(int i = 0 ; i < 100; i++)
+    {
+        // If shared memory has not yet created
+        if((shmid = shmget(shmem_key, sizeof(MasterList),0))== -1)
+        {
+            // sleep for 10 sec and try again
+            sleep(10);
+        }
+        else
+        {
+           while(1)
+           {
+               if(wod(shmid, semid) < 1)
+               {
+                   exit(4);
+               }
+           }
+        }       
+    }
+    
+    // remove semaphore
+    semctl (semid, 0, IPC_RMID,0);
+    
+    return (0);
+}
+
+
+int wod(int shmid, int semid)
+{
+    int     actionNum = 0;
+    int     r;
+    int     mid;
+    pid_t   pid;
+    char    temp[255];
+    MasterList  *p;
+
+    // attach to shared memory
+    p = (MasterList *)shmat (shmid, NULL, 0);
+    if(p == NULL)
+    {
+        dlog(DATA_CORRUPTOR,semid,"(DX)Cannot attach to shared memory!");
+        //printf("(DX)Cannot attach to shared memory!\n");
+        return -1;
+    }
+
+    actionNum++;
+    //Step1: sleep for a random amount of time (10 ~ 30)
+    r = (rand() % 21) + 10;
+    //printf("(Logger) sleep1 %d\n",r);
+    sleep(r);
+    //Step2 : check for the existance of the message queue
+    mid = p->msgQueueID;
+    //printf("(Logger) msgQueueID %d\n",mid);
+
+    if(mid == 0)
+    {
+        // detach from shared memory
+        shmdt (p);
+        // remove shared memory
+        shmctl(shmid,IPC_RMID,0);
+        //printf("DX deteched that msgQ is gone - assuming DR/DCs done\n");
+        // print log file
+        dlog(DATA_CORRUPTOR,semid,"DX deteched that msgQ is gone - assuming DR/DCs done");
+        // remove semaphore
+        semctl (semid, 0, IPC_RMID,0);
+
+        return 0;
+    }
+
+    // select random number for WOD
+    r = rand() % 3;
+
+    //WOD (Wheel of Destruction)
+    switch(r)
+    {
+        case DO_NOTING:
+            {
+                // log do notiong
+                //printf("do nothing\n");
+                dlog(DATA_CORRUPTOR,semid,"do nothing");
+                break;
+            }
+        case KILL_DC:
+            {
+                r = rand() % (p->numberOfDCs);
+                pid = (p->dc[r]).dcProcessID;
+                kill(pid, SIGHUP);
+                // log kill dc
+                //printf("WOD Action %02d - DC-%02d [%u] TERMINATED\n",actionNum, r, pid);
+                sprintf(temp,"WOD Action %02d - DC-%02d [%u] TERMINATED",actionNum, r, pid);
+                dlog(DATA_CORRUPTOR,semid,temp);
+                break;
+            }
+        case DELETE_MSGQ:
+            {
+                // log delete message queue
+                msgctl (mid, IPC_RMID, NULL);
+                //printf("DX deleted the msgQ - the DR/DCs can't talk anymore - exiting\n");
+                dlog(DATA_CORRUPTOR,semid,"DX deleted the msgQ - the DR/DCs can't talk anymore - exiting");
+                break;
+            }
+        default:
+            {
+                break;
+            }
+    }
+    return 1;
+}
